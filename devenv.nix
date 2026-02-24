cebdadf2bff8d879410f1c77338233e9e3727d66
lemisora <leonardo.michel.dom@gmail.com>
2026-02-23 20:37:59 -0600

    feat: added Nix devenv files and rules for Java development - Maven got added

---
+++ devenv.nix
@@ -0,0 +1,63 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
+
+{
+  # https://devenv.sh/basics/
+  # env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = with pkgs;[
+    # Dependencias para desarrollo con Java
+    # Si se debe usar paquetes que estén en Nixpkgs, se añaden acá
+    # sino usar los de Maven con el pom.xml
+  ];
+
+  languages.c.enable = true;
+  
+  # Habilitar Java para el devenv
+  languages.java = {
+    enable = true;
+    jdk.package = pkgs.jdk17;
+    
+    # Se usará el Maven de JDK
+    maven = {
+      enable = true;
+    };
+  };  
+  # https://devenv.sh/processes/
+  # processes.dev.exec = "${lib.getExe pkgs.watchexec} -n -- ls -la";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/scripts/
+  # scripts.hello.exec = ''
+  #   echo hello from $GREET
+  # '';
+
+  # https://devenv.sh/basics/
+  enterShell = ''
+  '';
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep --color=auto "${pkgs.git.version}"
+  '';
+
+  # https://devenv.sh/git-hooks/
+  # git-hooks.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
